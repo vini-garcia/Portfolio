ef5326647b7bd49826b1e8cae1cf95fd88b7c0c2
Vinicius Garcia <advviniciusgarcia@gmail.com>
2023-09-20 17:29:19 -0300

    Initial Commit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -47,7 +47,7 @@ export const Home = (): JSX.Element => {
               <Text color="grey4">Hello, my name is {userData.nameUser}</Text>
             </Flex>
             <Text as="h1" type="heading1" color="grey5">
-            I{" "}
+              I{" "}
               <Text as="span" type="heading1" color="brand1">
                 love
               </Text>{" "}
@@ -58,8 +58,8 @@ export const Home = (): JSX.Element => {
               projects
             </Text>
             <Text type="body1" color="grey2">
-              Discover here in this environment, created especially for you, all
-              my projects and technologies
+              A newly graduated Full Stack programmer, coming from 10 years working as a lawyer.
+              Every day improving my knowledge of soft and hard skills.
             </Text>
             <HeaderButtonsArea>
               <Button as="a" type="primary" href="#projects">
